e758da75e2e60c5c42ccba02a4c0ae4e27f4e661
Healer2202 <0962392354Hl>
2022-01-19 23:40:49 +0700

    update layout

--- src/sections/auth/reset-password/ResetPasswordForm.js
+++ src/sections/auth/reset-password/ResetPasswordForm.js
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import * as Yup from 'yup';
 import 'yup-phone';
+import { useState } from 'react';
+
 // form
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 // @mui
-
-import { Stack, Divider } from '@mui/material';
+import { Stack, IconButton, InputAdornment, Alert, Divider } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // hooks
 import useIsMountedRef from '../../../hooks/useIsMountedRef';
 // components
+import Iconify from '../../../components/Iconify';
 import { FormProvider, RHFTextField } from '../../../components/hook-form';
 
 // ----------------------------------------------------------------------
@@ -22,6 +24,10 @@ ResetPasswordForm.propTypes = {
 
 export default function ResetPasswordForm({ onSent, onGetPhone }) {
   const isMountedRef = useIsMountedRef();
+
+  const [showPassword2, setShowPassword2] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false);
 
   const ResetPasswordSchema = Yup.object().shape({
     phone: Yup.string().phone('VN', true, 'Số điện thoại không hợp lệ'),
@@ -52,6 +58,36 @@ export default function ResetPasswordForm({ onSent, onGetPhone }) {
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={3}>
         <RHFTextField name="phone" label="Số điện thoại" />
+
+        <RHFTextField
+          name="password"
+          label="Mật khẩu"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton edge="end" onClick={() => setShowPassword(!showPassword)}>
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <RHFTextField
+          name="passwordConfirmation"
+          label="Nhập lại mật khẩu"
+          type={showPassword2 ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton edge="end" onClick={() => setShowPassword2(!showPassword2)}>
+                  <Iconify icon={showPassword2 ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
 
         <Stack 
           direction={{ xs: 'column', sm: 'row' }} 
