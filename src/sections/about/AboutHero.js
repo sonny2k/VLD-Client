a561052f3115ca5ceb622faeb537a65a5a083886
tankhanh <dinhtankhanh14@gmail.com>
2022-05-24 12:09:25 +0700

    About Us

--- src/sections/about/AboutHero.js
+++ src/sections/about/AboutHero.js
@@ -11,7 +11,7 @@ const RootStyle = styled('div')(({ theme }) => ({
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundImage:
-    'url(https://minimal-assets-api.vercel.app/assets/overlay.svg), url(https://minimal-assets-api.vercel.app/assets/images/about/hero.jpg)',
+    'url(https://minimal-assets-api.vercel.app/assets/overlay.svg), url(https://nhakhoakim.com/wp-content/uploads/2021/10/BannerBS-6x-update-04.10.21-scaled.jpg)',
   padding: theme.spacing(10, 0),
   [theme.breakpoints.up('md')]: {
     height: 560,
@@ -34,12 +34,12 @@ export default function AboutHero() {
   return (
     <RootStyle>
       <Container component={MotionContainer} sx={{ position: 'relative', height: '100%' }}>
-        <ContentStyle>
-          <TextAnimate text="Who" sx={{ color: 'primary.main' }} variants={varFade().inRight} />
+        {/* <ContentStyle>
+          <TextAnimate text="VanLangDoctor" sx={{ color: 'Black' }} variants={varFade().inRight} />
           <br />
-          <Box sx={{ display: 'inline-flex', color: 'common.white' }}>
-            <TextAnimate text="we" sx={{ mr: 2 }} />
-            <TextAnimate text="are?" />
+          <Box sx={{ display: 'inline-flex', color: 'Black' }}>
+            <TextAnimate text="là" sx={{ mr: 2 }} />
+            <TextAnimate text="ai?" />
           </Box>
 
           <m.div variants={varFade().inRight}>
@@ -55,7 +55,7 @@ export default function AboutHero() {
               <br /> make awesome site easily
             </Typography>
           </m.div>
-        </ContentStyle>
+        </ContentStyle> */}
       </Container>
     </RootStyle>
   );
